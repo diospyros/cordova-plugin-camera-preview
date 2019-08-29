079e7f57cc79bc06d64b8da2937199e8a0e56170
diospyros <jwraymon@yahoo.com>
2019-08-29 16:50:11 -0400

    Update CameraActivity.java

--- src/android/CameraActivity.java
+++ src/android/CameraActivity.java
@@ -416,7 +416,7 @@ public class CameraActivity extends Fragment {
 //    cache = getActivity().getCacheDir();      // JWR
 //    cache = getActivity().getFilesDir();      //JWR
     cache = getActivity().getExternalFilesDir(null);
-//    cache = getActivity().getExternalStorageDirectory(null);  // JWR
+//    cache = MainActivity.this.getExternalFilesDir(null);  // JWR
 //    cache = getBaseActivity().getExternalFilesDir();  //JWR
 
     // Create the cache directory if it doesn't exist
