a58fc689ea20966737fc4122e3f4d346bd4ab4d7
diospyros <jwraymon@yahoo.com>
2019-09-03 10:14:20 -0400

    Update CameraActivity.java

--- src/android/CameraActivity.java
+++ src/android/CameraActivity.java
@@ -422,8 +422,9 @@ public class CameraActivity extends Fragment {
   }
   
   private void liberalizePermissions(String filePath ) {
-/*    
+  
     Path path = Paths.get(filePath);
+/*
     Set<PosixFilePermission> perms = Files.readAttributes(path,PosixFileAttributes.class).permissions();
 
 //    System.out.format("Permissions before: %s%n",  PosixFilePermissions.toString(perms));
