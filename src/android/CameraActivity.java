ec28d3e9ff550a32d9cf8842b3dc7d6492255829
diospyros <jwraymon@yahoo.com>
2019-09-27 10:04:10 -0400

    Update CameraActivity.java

--- src/android/CameraActivity.java
+++ src/android/CameraActivity.java
@@ -456,8 +456,8 @@ public class CameraActivity extends Fragment {
         
         // Process thumbnail    
 //        tnBitmap = createScaledBitmap ( tnBitmap, 100, 134, true );
-//        ByteArrayOutputStream tnOutputStream = new ByteArrayOutputStream();
-        //tnBitmap.compress(Bitmap.CompressFormat.JPEG, currentQuality, tnOutputStream);
+        ByteArrayOutputStream tnOutputStream = new ByteArrayOutputStream();
+        tnBitmap.compress(Bitmap.CompressFormat.JPEG, currentQuality, tnOutputStream);
 //        tnData = tnOutputStream.toByteArray();
 
         if (!storeToFile) {
